f59bf3ad5c39c83396c1650059073ef8340d9813
waelIsHasan <waelhasan12342@gmail.com>
2025-04-09 04:09:06 +0300

    Add some dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import { BrowserRouter , Routes , Route} from "react-router-dom";
-import { ApiProvider } from "./contexts/ApiContext.js";
-import{ApiTechProvider} from './contexts/ApiTechContext.js'
 import Home from './pages/Home/Home.jsx'
 import Tech from './pages/Tech/Tech.jsx'
 
